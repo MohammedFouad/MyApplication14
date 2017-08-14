ec27cffd3e815bfc48762ae8a8d6a6b6b20cd626
MohammedFouad <mohfoad@gmail.com>
2017-08-14 11:17:18 +0200

    Initial commit

---
+++ sampleapp/src/main/java/com/clockbyte/admobadapter/sampleapp/express/MainActivity_ListView_Express.java
@@ -0,0 +1,125 @@
+package com.clockbyte.admobadapter.sampleapp.express;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.clockbyte.admobadapter.expressads.AdmobExpressAdapterWrapper;
+import com.clockbyte.admobadapter.sampleapp.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.NativeExpressAdView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class MainActivity_ListView_Express extends Activity {
+
+    ListView lvMessages;
+    AdmobExpressAdapterWrapper adapterWrapper;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_listview);
+
+        //highly-recommended in Firebase docs to initialize things early as possible
+        //test_admob_app_id is different with unit_id! you could get it in your Admob console
+        MobileAds.initialize(getApplicationContext(), getString(R.string.test_admob_app_id));
+
+        initListViewItems();
+    }
+
+    /**
+     * Inits an adapter with items, wrapping your adapter with a {@link AdmobExpressAdapterWrapper} and setting the listview to this wrapper
+     * FIRST OF ALL Please notice that the following code will work on a real devices but emulator!
+     */
+    private void initListViewItems() {
+        lvMessages = (ListView) findViewById(R.id.lvMessages);
+
+        //creating your adapter, it could be a custom adapter as well
+        ArrayAdapter<String> adapter  = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1);
+
+        //your test devices' ids
+        String[] testDevicesIds = new String[]{getString(R.string.testDeviceID),AdRequest.DEVICE_ID_EMULATOR};
+        //when you'll be ready for release please use another ctor with admobReleaseUnitId instead.
+        adapterWrapper = new AdmobExpressAdapterWrapper(this, testDevicesIds){
+            @NonNull
+            @Override
+            protected ViewGroup getAdViewWrapper(ViewGroup parent) {
+                return (ViewGroup) LayoutInflater.from(parent.getContext()).inflate(R.layout.native_express_ad_container,
+                        parent, false);
+            }
+
+            @Override
+            protected void recycleAdViewWrapper(@NonNull ViewGroup wrapper, @NotNull NativeExpressAdView ad) {
+                //get the view which directly will contain ad
+                ViewGroup container = (ViewGroup) wrapper.findViewById(R.id.ad_container);
+                /**iterating through all children of the container view and remove the first occured {@link NativeExpressAdView}. It could be different with {@param ad}!!!*/
+                for (int i = 0; i < container.getChildCount(); i++) {
+                    View v = container.getChildAt(i);
+                    if (v instanceof NativeExpressAdView) {
+                        container.removeViewAt(i);
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            protected void addAdViewToWrapper(@NonNull ViewGroup wrapper, @NotNull NativeExpressAdView ad) {
+                //get the view which directly will contain ad
+                ViewGroup container = (ViewGroup) wrapper.findViewById(R.id.ad_container);
+                /**add the {@param ad} directly to the end of container*/
+                container.addView(ad);
+            }
+        };
+        //By default the ad size is set to FULL_WIDTHx150
+        //To set a custom size you should use an appropriate ctor
+        //adapterWrapper = new AdmobExpressAdapterWrapper(this, testDevicesIds, new AdSize(AdSize.FULL_WIDTH, 150));
+
+        adapterWrapper.setAdapter(adapter); //wrapping your adapter with a AmobExpressAdapterWrapper.
+
+        //Sets the max count of ad blocks per dataset, by default it equals to 3 (according to the Admob's policies and rules)
+        adapterWrapper.setLimitOfAds(10);
+
+        //Sets the number of your data items between ad blocks, by default it equals to 10.
+        //You should set it according to the Admob's policies and rules which says not to
+        //display more than one ad block at the visible part of the screen,
+        // so you should choose this parameter carefully and according to your item's height and screen resolution of a target devices
+        adapterWrapper.setNoOfDataBetweenAds(10);
+
+        adapterWrapper.setFirstAdIndex(2);
+
+        lvMessages.setAdapter(adapterWrapper); // setting an AdmobAdapterWrapper to a ListView
+
+        //preparing the collection of data
+        final String sItem = "item #";
+        ArrayList<String> lst = new ArrayList<String>(100);
+        for(int i=1;i<=100;i++)
+            lst.add(sItem.concat(Integer.toString(i)));
+
+        //adding a collection of data to your adapter and rising the data set changed event
+        adapter.addAll(lst);
+        adapter.notifyDataSetChanged();
+    }
+
+    /*
+    * Seems to be a good practice to destroy all the resources you have used earlier :)
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        adapterWrapper.release();
+    }
+}
